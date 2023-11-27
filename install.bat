9cd51b2af227d7171f8f8aac041253660be71621
Yui <dev@sleepyyui.com>
2023-11-27 16:59:53 +0100

    update install.bat

--- install.bat
+++ install.bat
@@ -11,6 +11,35 @@ REM upgrade pip
 echo Upgrading pip...
 %PYTHON% -m pip install --upgrade pip
 
+REM Checking for espeak
+echo Checking if espeak header exists in C:\Program Files\expeak\speak_lib.h
+if exist "C:\Program Files\espeakHeaders" (
+    echo Found expeak header!
+) else (
+    echo Could not find expeak header!
+    echo Installing espeak from source...
+    REM Copying espeak directory to C:\Program Files\espeak
+    xcopy /E /H /I /Y espeakWin "C:\Program Files\espeakHeaders"
+    echo Copied espeak header!
+
+    REM Checking if folder creation was successful
+    echo Checking if folder creation was successful...
+    if exist "C:\Program Files\espeakHeaders" (
+        echo Folder creation was successful!
+    ) else (
+        echo Folder creation was not successful!
+        echo Please try again! If this error persists, try running this script as administrator or copy the espeakWin directory to C:\Program Files\espeakHeaders
+        pause
+        exit
+    )
+)
+
+:: REM Checking if espeak directory is in path
+:: echo Checking if espeak directory is in path...
+:: setx path "%path%;C:\Program Files\espeakHeaders" /M
+:: echo Added espeak directory to path!
+
+
 REM install requirements
 echo Installing requirements...
 %PYTHON% -m pip install -r requirements.txt
